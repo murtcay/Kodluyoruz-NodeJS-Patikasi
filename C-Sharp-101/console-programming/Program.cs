62b8d724874c5e33fec26d9e5d5d7bd3fea36583
Murat ÇAY <murtcay@gmail.com>
2022-01-03 20:50:29 +0300

    Csharp 101: console project

---
+++ C-Sharp-101/console-programming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Lastname: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Helo, " + name + " " + lastname);
+        }
+    }
+}
