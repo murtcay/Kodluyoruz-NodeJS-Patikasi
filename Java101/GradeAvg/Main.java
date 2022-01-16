e17f01d753d0e8bf73725e1e2dd1fbafab721c34
Murat ÇAY <murtcay@gmail.com>
2022-01-16 19:52:56 +0300

    Java101: average grade calculation with ternary operator.

--- Java101/GradeAvg/Main.java
+++ Java101/GradeAvg/Main.java
@@ -12,38 +12,34 @@ public class Main {
     
     System.out.print("Grade of Math Course : ");
     gradeMath = input.nextInt();
-    if(gradeMath > 100)  gradeMath = 100;
+    gradeMath = (gradeMath > 100) ? 100 : gradeMath;
     
     System.out.print("Grade of Physics Course : ");
     gradePhysics = input.nextInt();
-    if(gradePhysics > 100)  gradePhysics = 100;
+    gradePhysics = (gradePhysics > 100) ? 100 : gradePhysics;
     
     System.out.print("Grade of Chemistry Course : ");
     gradeChemistry = input.nextInt();
-    if(gradeChemistry > 100)  gradeChemistry = 100;
+    gradeChemistry = (gradeChemistry > 100) ? 100 : gradeChemistry;
     
     System.out.print("Grade of Turkish Course : ");
     gradeTurkish = input.nextInt();
-    if(gradeTurkish > 100)  gradeTurkish = 100;
+    gradeTurkish = (gradeTurkish > 100) ? 100 : gradeTurkish;
     
     System.out.print("Grade of History Course : ");
     gradeHistory = input.nextInt();
-    if(gradeHistory > 100)  gradeHistory = 100;
+    gradeHistory = (gradeHistory > 100) ? 100 : gradeHistory;
     
     System.out.print("Grade of Music Course : ");
     gradeMusic = input.nextInt();
-    if(gradeMusic > 100)  gradeMusic = 100;
+    gradeMusic = (gradeMusic > 100) ? 100 : gradeMusic;
     
     input.close();
 
     int avg = (gradeMath + gradePhysics + gradeChemistry + gradeTurkish + gradeHistory + gradeMusic)/6;
     System.out.println("\nAverage: " + avg);
     
-    if(avg > 60) {
-      System.out.println("Passed.");
-    }
-    else {
-      System.out.println("Failed.");
-    }
+    String resp = (avg > 60) ? "Passed." : "Failed.";
+    System.out.println(resp);
   }
 }
