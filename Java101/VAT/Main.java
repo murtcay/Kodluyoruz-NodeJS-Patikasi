867ee1400b2ce49649a51efb56483de396106176
Murat ÇAY <murtcay@gmail.com>
2022-01-16 20:08:58 +0300

    Java101 - VAT calculator program

---
+++ Java101/VAT/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner inputPrice = new Scanner(System.in);
+    
+    System.out.print("Please enter total price: ");
+    double price = inputPrice.nextDouble();
+
+    double vatRatio = (price < 1000) ? 0.18 : 0.08;
+    double fee = vatRatio * price;
+    double totalPrice = price + fee;
+
+    System.out.println("Price: " + price);
+    System.out.println("VAT ratio: " + vatRatio);
+    System.out.println("Fee: " + fee);
+    System.out.println("Total Amount: " + totalPrice);
+
+  }
+}
---
+++ Java101/VAT/README.md
@@ -0,0 +1,3 @@
+## VAT Calculator Program
+
+The program that calculates the VAT price of the money value received from the user with Java and prints it on the screen. If the amount entered is between 0 and 1000 TL, the VAT rate is 18%, if the amount is greater than 1000 TL, the VAT amount is calculated as 8%.
